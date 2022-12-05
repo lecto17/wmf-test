327351ab9ecee6dc432264fd127e177e3486a5de
lecto17 <khwoo_rpsit@naver.com>
2022-12-05 15:52:58 +0900

    remote 환경에서 styled-component 붙인 컴포넌트 로드 시 발생하는 에러 수정 중

--- host/src/App.js
+++ host/src/App.js
@@ -1,15 +1,16 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 
-const Counter = React.lazy(() => import('app1/Counter'))
+const Counter = React.lazy(() => import('app1/Counter'));
+const Button = React.lazy(() => import('app1/Button'));
 
 function App() {
   return (
-    <div className="wmf-test">3
+    <div className="wmf-test">
     
       <h1>Hello, this Componet is From Remote</h1>
       <Suspense fallback="loading Component from remote, please wait for loading...">
-        <Counter />
+        <Button />
       </Suspense>
     </div>
   );
