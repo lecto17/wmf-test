8db81d01aadba180ce0e0cb4c5fb863bbadf47f5
lecto17 <khwoo_rpsit@naver.com>
2022-12-05 11:18:19 +0900

    import 경로 수정

--- host/src/App.js
+++ host/src/App.js
@@ -1,11 +1,12 @@
 import React, { Suspense } from "react";
-import ReactDOM from "react";
+import ReactDOM from "react-dom";
 
 const Counter = React.lazy(() => import('app1/Counter'))
 
 function App() {
   return (
-    <div className="wmf-test">
+    <div className="wmf-test">3
+    
       <h1>Hello, this Componet is From Remote</h1>
       <Suspense fallback="loading Component from remote, please wait for loading...">
         <Counter />
