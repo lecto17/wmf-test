96c6e0c879f42300355ac383fe743c045d81fa5a
lecto17 <khwoo_rpsit@naver.com>
2022-12-09 18:15:56 +0900

    컴포넌트에서 사용되는 theme 추가

---
+++ remote/src/theme/theme.js
@@ -0,0 +1,34 @@
+const componentTheme = {
+    palette: {
+        /* blue */
+        blue1: "#A4BBE5",
+        blue2: "#87A6DD",
+        blue3: "#6A93D2",
+        blue4: "#4F7DCB",
+        blue5: "#3469C2",
+        blue6: "#2D5AA5",
+        blue7: "#254B8A",
+        blue8: "#1C3C72",
+        blue9: "#152E52",
+        blue10: "#0D1E38",
+
+        primaryBlack: "#222A37",
+    },
+    bg: {
+        /* background-color */
+        bg1: "#FFFFFF",
+        bg2: "#F5F6F8",
+        bg3: "#EAECF1",
+        bg4: "#D7DAE2",
+        bg5: "#C8CCD5",
+        bg6: "#A4ABBB",
+        bg7: "#8391AC",
+        bg8: "#61708B",
+        bg9: "#48566E",
+        bg10: "#333F55",
+        bg11: "#222A37",
+        bg12: "#242424",
+    }
+}
+
+export default componentTheme;
